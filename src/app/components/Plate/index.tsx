fe4b11a6ca257d6e08b28791ed03726a803c9280
Kamil <kamilprogramuje@gmail.com>
2024-02-25 16:20:35 +0100

    handlemouse down works correctly now

--- src/app/components/Plate/index.tsx
+++ src/app/components/Plate/index.tsx
@@ -3,19 +3,6 @@
 import React, { FC } from "react";
 import cn from "classnames";
 
-// import blackPawn from "@/app/images/black/black_pawn.png";
-// import whitePawn from "@/app/images/white/white_pawn.png";
-// import blackRook from "@/app/images/black/black_rook.png";
-// import whiteRook from "@/app/images/white/white_rook.png";
-// import blackKnight from "@/app/images/black/black_knight.png";
-// import whiteKnight from "@/app/images/white/white_knight.png";
-// import blackBishop from "@/app/images/black/black_bishop.png";
-// import whiteBishop from "@/app/images/white/white_bishop.png";
-// import blackQueen from "@/app/images/black/black_queen.png";
-// import whiteQueen from "@/app/images/white/white_queen.png";
-// import blackKing from "@/app/images/black/black_king.png";
-// import whiteKing from "@/app/images/white/white_king.png";
-// import Image from "next/image";
 enum Pieces {
   BLACKPAWN = "black black__pawn",
   WHITEPAWN = "white white__pawn",
@@ -41,35 +28,35 @@ type Props = {
 };
 
 const Plate: FC<Props> = ({ coords: { x, y }, indexX, indexY }) => {
-  let image = null;
+  let currentPiece = null;
 
   if (indexY === 1) {
-    image = Pieces.BLACKPAWN;
+    currentPiece = Pieces.BLACKPAWN;
   }
 
   if (indexY === 6) {
-    image = Pieces.WHITEPAWN;
+    currentPiece = Pieces.WHITEPAWN;
   }
 
   if (indexY === 0) {
     switch (indexX) {
       case 7:
       case 0:
-        image = Pieces.BLACKROOK;
+        currentPiece = Pieces.BLACKROOK;
         break;
       case 6:
       case 1:
-        image = Pieces.BLACKNIGHT;
+        currentPiece = Pieces.BLACKNIGHT;
         break;
       case 5:
       case 2:
-        image = Pieces.BLACKBISHOP;
+        currentPiece = Pieces.BLACKBISHOP;
         break;
       case 3:
-        image = Pieces.BLACKQUEEN;
+        currentPiece = Pieces.BLACKQUEEN;
         break;
       case 4:
-        image = Pieces.BLACKKING;
+        currentPiece = Pieces.BLACKKING;
         break;
       default:
         break;
@@ -80,21 +67,21 @@ const Plate: FC<Props> = ({ coords: { x, y }, indexX, indexY }) => {
     switch (indexX) {
       case 7:
       case 0:
-        image = Pieces.WHITEROOK;
+        currentPiece = Pieces.WHITEROOK;
         break;
       case 6:
       case 1:
-        image = Pieces.WHITEKNIGHT;
+        currentPiece = Pieces.WHITEKNIGHT;
         break;
       case 5:
       case 2:
-        image = Pieces.WHITEBISHOP;
+        currentPiece = Pieces.WHITEBISHOP;
         break;
       case 3:
-        image = Pieces.WHITEQUEEN;
+        currentPiece = Pieces.WHITEQUEEN;
         break;
       case 4:
-        image = Pieces.WHITEKING;
+        currentPiece = Pieces.WHITEKING;
         break;
       default:
         break;
@@ -109,31 +96,19 @@ const Plate: FC<Props> = ({ coords: { x, y }, indexX, indexY }) => {
       className={cn(
         "portrait:w-[12vw]",
         "portrait:h-[12vw]",
+        "portrait:text-[2vw]",
         "landscape:w-[12vh]",
         "landscape:h-[12vh]",
+        "landscape:text-[2vh]",
         "relative",
         (indexX + indexY) % 2 === 0 && "bg-green-800",
         (indexX + indexY) % 2 === 0 && "text-white"
       )}
+      onMouseDown={handleMouseDown}
     >
-      <span className="absolute">{indexX === 0 && y}</span>
-      <span className="absolute right-0 bottom-0">{indexY === 7 && x}</span>
-      {image && (
-        <div className={image}></div>
-        // <Image
-        //   onMouseDown={handleMouseDown}
-        //   src={image}
-        //   alt="piece"
-        //   priority={true}
-        //   style={{
-        //     position: "absolute",
-        //     top: "50%",
-        //     left: "50%",
-        //     transform: "translate(-50%, -50%)",
-        //     cursor: "pointer",
-        //   }}
-        // />
-      )}
+      <span className="absolute left-0.5 top-0.5">{indexX === 0 && y}</span>
+      <span className="absolute right-0.5 bottom-0.5">{indexY === 7 && x}</span>
+      {currentPiece && <div className={currentPiece}></div>}
     </div>
   );
 };
